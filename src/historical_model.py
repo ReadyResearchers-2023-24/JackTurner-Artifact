9b9c2cfb47dc94f084a4a0680916c28887183297
Jack Turner <turner01@allegheny.edu>
2023-10-13 14:13:08 -0400

    reformatted code with black

--- src/historical_model.py
+++ src/historical_model.py
@@ -6,27 +6,29 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 # Define the stock symbol and date range
-stock_symbol = 'AAPL'
-start_date = '2020-01-01'
-end_date = '2021-12-31'
+stock_symbol = "AAPL"
+start_date = "2020-01-01"
+end_date = "2021-12-31"
 
 # Fetch historical stock price data using yfinance
 data = yf.download(stock_symbol, start=start_date, end=end_date)
 
 # Select the 'Adj Close' column as the stock price
-df = data[['Adj Close']].reset_index()
+df = data[["Adj Close"]].reset_index()
 
 # Feature engineering
 for i in range(1, 6):
-    df[f'Lag_{i}'] = df['Adj Close'].shift(i)
+    df[f"Lag_{i}"] = df["Adj Close"].shift(i)
 
 # Drop rows with missing values (NaN) introduced by lagging
 df = df.dropna()
 
 # Split the data into training and testing sets
-X = df.drop(['Date', 'Adj Close'], axis=1)
-y = df['Adj Close']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X = df.drop(["Date", "Adj Close"], axis=1)
+y = df["Adj Close"]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 # Create and train a linear regression model
 model = LinearRegression()
@@ -37,18 +39,19 @@ y_pred = model.predict(X_test)
 
 # Evaluate the model
 from sklearn.metrics import mean_squared_error, r2_score
+
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
-print(f'Mean Squared Error: {mse}')
-print(f'R-squared (R2) Score: {r2}')
+print(f"Mean Squared Error: {mse}")
+print(f"R-squared (R2) Score: {r2}")
 
 # Visualize the predicted vs. actual prices
 plt.figure(figsize=(12, 6))
-plt.plot(df['Date'][-len(y_test):], y_test.values, label='Actual Price', color='blue')
-plt.plot(df['Date'][-len(y_test):], y_pred, label='Predicted Price', color='red')
-plt.xlabel('Date')
-plt.ylabel('Stock Price')
-plt.title(f'Stock Price Prediction for {stock_symbol}')
+plt.plot(df["Date"][-len(y_test) :], y_test.values, label="Actual Price", color="blue")
+plt.plot(df["Date"][-len(y_test) :], y_pred, label="Predicted Price", color="red")
+plt.xlabel("Date")
+plt.ylabel("Stock Price")
+plt.title(f"Stock Price Prediction for {stock_symbol}")
 plt.legend()
 plt.show()
--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -21,28 +21,35 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 
+
 def get_html_content(url):
     # get HTML content from url
-    req = Request(url=url, headers={
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'})
+    req = Request(
+        url=url,
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+        },
+    )
     response = urlopen(req)
     return response.read()
 
+
 def get_news_tables(tickers):
     # get news table for each ticker
-    site_url = 'https://finviz.com/quote.ashx?t='
+    site_url = "https://finviz.com/quote.ashx?t="
     news_tables = {}
     for ticker in tickers:
         url = site_url + ticker
         html = BeautifulSoup(get_html_content(url), features="html.parser")
-        news_table = html.find(id='news-table')
+        news_table = html.find(id="news-table")
         news_tables[ticker] = news_table
     return news_tables
+
 
 def parse_news(news_tables):
     parsed_news = []
     for file_name, news_table in news_tables.items():
-        for x in news_table.findAll('tr'):
+        for x in news_table.findAll("tr"):
             text = x.get_text()
             date_scrape = x.td.text.split()
             if len(date_scrape) == 1:
@@ -50,13 +57,14 @@ def parse_news(news_tables):
             else:
                 date = date_scrape[0]
                 time = date_scrape[1]
-            ticker = file_name.split('_')[0]
+            ticker = file_name.split("_")[0]
             parsed_news.append([ticker, date, time, text])
     return parsed_news
 
+
 def analyze_sentiment(tickers):
     # download NLTK data
-    nltk.download('vader_lexicon')
+    nltk.download("vader_lexicon")
 
     # get news tables
     news_tables = get_news_tables(tickers)
@@ -66,12 +74,12 @@ def analyze_sentiment(tickers):
 
     # analyze sentiment
     vader = SentimentIntensityAnalyzer()
-    c = ['ticker', 'date', 'time ', 'headline']
+    c = ["ticker", "date", "time ", "headline"]
     parsed_and_scored_news = pd.DataFrame(parsed_news, columns=c)
-    scores = parsed_and_scored_news['headline'].apply(vader.polarity_scores).tolist()
+    scores = parsed_and_scored_news["headline"].apply(vader.polarity_scores).tolist()
     scores_df = pd.DataFrame(scores)
-    parsed_and_scored_news = parsed_and_scored_news.join(scores_df, rsuffix='_right')
-    parsed_and_scored_news['date'] = pd.to_datetime(parsed_and_scored_news.date).dt.date
+    parsed_and_scored_news = parsed_and_scored_news.join(scores_df, rsuffix="_right")
+    parsed_and_scored_news["date"] = pd.to_datetime(parsed_and_scored_news.date).dt.date
 
     # create a sentiment dictionary to hold the sentiment score of each ticker
     sentiment_dict = {}
@@ -80,9 +88,9 @@ def analyze_sentiment(tickers):
 
     # display results
     for index, row in parsed_and_scored_news.iterrows():
-        ticker = row['ticker']
-        sentiment_score = row['compound']
-        headline = row['headline']
+        ticker = row["ticker"]
+        sentiment_score = row["compound"]
+        headline = row["headline"]
 
         print(f"Ticker: {ticker}")
         print(f"Headline: {headline}")
@@ -98,8 +106,11 @@ def analyze_sentiment(tickers):
     for ticker, sentiment_score in sentiment_dict.items():
         print(f"Overall Sentiment Score for {ticker}: {sentiment_score:.2f}")
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Financial News Sentiment Analysis')
-    parser.add_argument('tickers', nargs='+', help='List of tickers separated by spaces')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Financial News Sentiment Analysis")
+    parser.add_argument(
+        "tickers", nargs="+", help="List of tickers separated by spaces"
+    )
     args = parser.parse_args()
     analyze_sentiment(args.tickers)
