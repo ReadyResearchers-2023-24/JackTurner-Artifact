b21641727d9b50bfef40b817bdc593248e7ddf5e
jackturner83 <79563663+jackturner83@users.noreply.github.com>
2023-10-13 19:55:26 -0400

    Ran black linter

--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -6,28 +6,30 @@ from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 
 # Initialize NLTK
-nltk.download('vader_lexicon')
+nltk.download("vader_lexicon")
 
 # Define functions
 def get_financial_news(url):
-    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    req = Request(url, headers={"User-Agent": "Mozilla/5.0"})
     response = urlopen(req)
     html = response.read()
-    soup = BeautifulSoup(html, 'html.parser')
-    headlines = soup.find_all('h2', class_='financial-news-headline')
+    soup = BeautifulSoup(html, "html.parser")
+    headlines = soup.find_all("h2", class_="financial-news-headline")
     return [headline.text for headline in headlines]
+
 
 def analyze_sentiment(headline):
     analyzer = SentimentIntensityAnalyzer()
     sentiment = analyzer.polarity_scores(headline)
     return sentiment
 
+
 def main():
     # Read the CSV file with the format "headline, url, publisher, date, stock"
-    df = pd.read_csv('src/300_stock_headlines.csv')
+    df = pd.read_csv("src/300_stock_headlines.csv")
 
     # Apply sentiment analysis to headlines
-    df['sentiment'] = df['headline'].apply(analyze_sentiment)
+    df["sentiment"] = df["headline"].apply(analyze_sentiment)
 
     # Display results
     for index, row in df.iterrows():
@@ -36,7 +38,8 @@ def main():
         print()
 
     # Save results to a new CSV file
-    df.to_csv('sentiment_analysis_results.csv', index=False)
+    df.to_csv("sentiment_analysis_results.csv", index=False)
+
 
 if __name__ == "__main__":
     main()
